2b42921024b40f90a729cdc7c7832b5bb12bf426
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-07-09 12:16:59 +0530

    center routes

--- src/routers/center.js
+++ src/routers/center.js
@@ -1,3 +1,15 @@
-/**
- * Created by apoorvaa_gupta on 9/7/17.
- */
+const router = require('express').Router();
+// get all the centers
+router.get('/', function (req,res) {
+
+});
+
+//add any center
+router.post('/add', function () {
+
+});
+
+// delete any center
+router.delete('/delete/:id', function () {
+
+})
