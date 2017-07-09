c776fee45c0f9a91807582419807f6022ea73e2b
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-07-09 12:17:33 +0530

    courses routes

--- src/routers/course.js
+++ src/routers/course.js
@@ -1,1 +1,16 @@
+const router = require('express').Router();
+// get all the courses
+router.get('/', function (req,res) {
+
+});
+
+//add any course
+router.post('/add', function () {
+
+});
+
+// delete any course
+router.delete('/delete/:id', function () {
+
+})
 
