9e24dbc95f16b4c16a9a234308e0d584f140f2c8
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-07-09 12:15:53 +0530

    lead routes

--- src/routers/lead.js
+++ src/routers/lead.js
@@ -1,3 +1,25 @@
-/**
- * Created by apoorvaa_gupta on 9/7/17.
- */
+const router = require('express').Router();
+// get all the leads
+router.get('/', function (req,res) {
+
+});
+
+// get details of a particular lead
+router.get('/:id', function () {
+
+});
+
+//add any lead
+router.post('/add', function () {
+
+});
+
+//edit details of the lead
+router.put('/edit/:id', function () {
+
+});
+
+// delete any lead
+router.delete('/delete/:id', function () {
+
+})
