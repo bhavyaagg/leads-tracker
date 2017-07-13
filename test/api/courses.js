29333ce92bd4b554c1bd43906e390042864c7feb
Arnav Gupta <arnav@codingblocks.com>
2017-07-13 19:10:13 +0530

    setup coverage
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- test/api/courses.js
+++ test/api/courses.js
@@ -15,6 +15,25 @@ it("POST /add should add a course", (done) => {
     name: 'Launchpad'
   }).end((e,r) => {
     r.statusCode.should.equal(201)
+    r.body.success.should.equal(true)
+    done()
+  })
+})
+
+it("GET /1 should fetch Launchpad", (done) => {
+  api.get('/1').end((e,r) => {
+    r.statusCode.should.equal(200)
+    r.body.success.should.equal(true)
+    r.body.data.name.should.equal('Launchpad')
+    done()
+  })
+})
+
+it("GET /2 should fetch error", (done) => {
+  api.get('/2').end((e,r) => {
+    r.statusCode.should.equal(404)
+    r.body.success.should.equal(false)
+    r.body.error.message.should.be.a('string')
     done()
   })
 })
