c6070ffca6980b27149e52b8ea211265374a788f
Arnav Gupta <arnav@codingblocks.com>
2017-08-16 19:54:26 +0530

    fix for heroku
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- src/db/models.ts
+++ src/db/models.ts
@@ -1,16 +1,8 @@
 import Sequelize, {Model} from 'sequelize'
+const CONFIG = require('../../config')
 
-var secrets;
-try {
-  secrets = require('./../../secrets.json')
-} catch (e) {
-  console.error('Create your own secrets file lazybones');
-  secrets = require('./../../secret-sample.json');
-}
 
-const DATABASE_URL = process.env.DATABASE_URL || ('postgres://' + secrets.DB_USER + ":" + secrets.DB_PASSWORD + "@" + secrets.DB_HOST + ":5432/" + secrets.DATABASE);
-
-const db = new Sequelize(DATABASE_URL);
+const db = new Sequelize(CONFIG.DATABASE_URL);
 
 const User = db.define('user', {
   id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true}
